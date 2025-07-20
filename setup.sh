2a0ad016b7acaeabb8083cd74ea961a2f4753936
JustusFriehl <justusfriehl@gmail.com>
2025-07-20 14:46:20 +0200

    I hope This Works

--- setup.sh
+++ setup.sh
@@ -39,5 +39,20 @@ else
 fi
 echo "Confiuring Firefox"
 cp -r mozilla ~/.mozilla
-echo "Assembling Distrobox"
-distrobox assemble create
+echo "Installing Nix"
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+echo "Installing Flatpak"
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo "Setting up Github-Cli"
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
+echo "Finished"
